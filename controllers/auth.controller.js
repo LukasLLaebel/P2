9cbe81b8ca4aa78882b053425ed8fa5c4c3084ee
lukas leander laebel <lukaslaebel@gmail.com>
2026-04-14 17:52:52 +0200

    changed up the structure to use .ejs files instead to make the file system more dynamic

--- controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-import authService from '../services/auth.service.js';
-
-export default {
-  getAuth: (req, res) => {
-    const data = authService.getAuthInfo();
-    res.json(data);
-  }
-};
-
--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -6,24 +6,35 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-router.get("/login", (req, res) => {
+
+router.use((req, res, next) => {
+  res.locals.currentPage = req.path;
+  next();
+});
+
+
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
 router.get('/all', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/all-files.html'));
+  res.render('../views/all-files.ejs', {
+    user: req.user
+  });
 });
 
 router.get("/shared", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/shared_with_me.html"));
+  res.render('../views/shared-with-me.ejs', {
+    user: req.user
+  });
 });
 
 router.get('/owned', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/my-files.html'));
+  res.render('../views/my-files.ejs', { user: req.user });
 });
 
 router.get('/roles', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/make-roles.html'));
+  res.render('../views/roles.ejs', { user: "Jeff" });
 });
 
 export default router;
