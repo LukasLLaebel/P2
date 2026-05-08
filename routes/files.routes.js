3c5df5fa09c2333e6352f1b18509d87914e71ad3
Laurafh99 <lfhe23@student.aau.dk>
2026-05-08 11:56:18 +0200

    + new file button adds new file to json and filesystem - still an error message when adding 2nd file.

--- routes/files.routes.js
+++ routes/files.routes.js
@@ -40,4 +40,65 @@ router.get("/getFolder/:id", (req, res) => {
   res.json(folder);
 });
 
+// Creates the new folder
+router.post('/create', requireAuth, async (req, res) => {
+  try {
+    const { file, shareId } = req.body;
+
+    // Validates input name
+    if (!file || file.trim() === "") {
+      return res.status(400).json({
+        success: false,
+        message: 'File name cannot be empty'
+      });
+    }
+
+    const authData = JSON.parse(fs.readFileSync(DBFilePath, 'utf-8'));
+
+    //Locates selected share
+    const share = authData.shares.find(s => s.id === Number(shareId));
+
+    if (!share) {
+      return res.status(404).json({
+        success: false,
+        message: 'Folder not found'
+      });
+    }
+
+    // Checks that "files" exists
+    if (!share.files) {
+      share.files = [];
+    }
+
+    // Creates new file
+    const newFile = file.trim()
+    share.files.push(newFile);
+
+    //Adds file to filesystem
+    const sharesPath = path.join(process.cwd(), 'shares');
+    const userFolderPath = path.join(sharesPath, share.owner);
+    const shareFolderPath = path.join(userFolderPath, share.name);
+    await fs.promises.mkdir(userFolderPath, { recursive: true });
+    await fs.promises.mkdir(shareFolderPath, { recursive: true, force: true });
+    const newFilePath = path.join(shareFolderPath, file.trim());
+    await fs.promises.writeFile(newFilePath, '');
+
+    fs.writeFileSync(DBFilePath, JSON.stringify(authData, null, 2));
+
+    res.json({
+      success: true,
+      message: 'File created successfully',
+      file: newFile
+    });
+
+  } catch (error) {
+    console.error('Error creating file:', error);
+    res.status(500).json({
+      success: false,
+      message: 'Error creating file',
+      error: error.message
+    });
+  }
+});
+
 export default router;
